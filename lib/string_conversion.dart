6545efaf82f54cda9724aae23edfc48e7cc58423
Igor Naigovzin <nashnub@gmail.com>
2022-04-15 10:38:12 +0300

    exercise 2.3 fixes

--- lib/string_conversion.dart
+++ lib/string_conversion.dart
@@ -2,11 +2,18 @@ class StringCoversion {
   List<num> stringToNums(String str) {
     final result = <num>[];
     final tokens = str.split(' ');
-    tokens
-        .map((e) => {
-              if (num.tryParse(e) != null) {result.add(num.parse(e))}
-            })
-        .toList();
+    for (var token in tokens) {
+      final number = num.tryParse(token);
+      if (number != null) {
+        result.add(number);
+      }
+    }
+    // Commented out due to double parsing
+    // tokens
+    //     .map((e) => {
+    //           if (num.tryParse(e) != null) {result.add(num.parse(e))}
+    //         })
+    //     .toList();
     return result;
   }
 }
